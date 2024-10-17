b5225d8e9b8aa332e12dc884286c82ce7725fc9c
Katie Lonsdale <katie952@gmail.com>
2024-10-17 12:25:10 -0600

    initialize SessionManager in MainActivity

--- app/src/main/java/com/example/innercircles/MainActivity.kt
+++ app/src/main/java/com/example/innercircles/MainActivity.kt
@@ -11,6 +11,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        todo: is this being initialized twice?
+        SessionManager.init(this)
         setContent {
             MaterialTheme {
                 MainScreen()
