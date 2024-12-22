53c14bd15a63cc46b8051d6138dd651eefb25426
Katie Lonsdale <katie952@gmail.com>
2024-12-22 14:34:04 -0700

    add user state and model

---
+++ app/src/main/java/com/example/innercircles/api/data/UserUiState.kt
@@ -0,0 +1,9 @@
+package com.example.innercircles.api.data
+
+data class UserUiState (
+        val id: String = "",
+        val email: String = "",
+        val displayName: String = "",
+        val notificationFrequency: String = "",
+        val lastTouAcceptance: String = "",
+)
