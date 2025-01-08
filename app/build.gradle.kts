d91318363e94917bc239c615a3d86bdf5ff33a0e
katielonsdale <katie952@gmail.com>
2025-01-07 18:39:19 -0700

    update versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.katielonsdale.chatterbox"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.1"
+        versionCode = 7
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
