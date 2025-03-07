adfd1c535509bb664414a4d58e5b57a3b1abdd39
Katie Lonsdale <katie952@gmail.com>
2025-03-07 14:10:49 -0700

    update for release and ignore signed app

--- app/src/main/java/com/katielonsdale/chatterbox/api/ApiClient.kt
+++ app/src/main/java/com/katielonsdale/chatterbox/api/ApiClient.kt
@@ -6,8 +6,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://10.0.2.2:3000/api/v0/"
-//    private const val BASE_URL = "https://chatter-box-be-c1487dd4c370.herokuapp.com/api/v0/"
 
     fun createApiService(sessionManager: SessionManager): ApiService {
         val okHttpClient = OkHttpClient.Builder()
