7f99fff359d2c8a45d7791030f556aa070a32532
katielonsdale <katie952@gmail.com>
2025-06-16 15:19:47 -0600

    set up theme files

---
+++ app/src/main/java/com/katielonsdale/chatterbox/theme/ChatterBoxTheme.kt
@@ -0,0 +1,44 @@
+package com.katielonsdale.chatterbox.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Typography
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+
+val LightColorTheme = lightColorScheme(
+    primary = Primary,
+    surface = Surface,
+    surfaceVariant = SurfaceVariant,
+    onSurface = OnSurface,
+    onSurfaceVariant = OnSurfaceVariant,
+    background = Background,
+    onBackground = OnBackground,
+)
+
+//@Composable
+//fun extendedColor(light: Color, dark: Color): Color {
+//    return if(isSystemInDarkTheme()) dark else light
+//}
+
+val Shapes = Shapes(
+    extraSmall = RoundedCornerShape(10.dp),
+    small = RoundedCornerShape(10.dp),
+    medium = RoundedCornerShape(10.dp),
+    large = RoundedCornerShape(10.dp),
+    extraLarge = RoundedCornerShape(10.dp)
+)
+
+@Composable
+fun ChatterBoxTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = LightColorTheme,
+        typography = Typography,
+        shapes = Shapes,
+        content = content,
+    )
+}
---
+++ app/src/main/java/com/katielonsdale/chatterbox/theme/Colors.kt
@@ -0,0 +1,17 @@
+package com.katielonsdale.chatterbox.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Background = Color(0xFFF3FBFC)
+val OnBackground = Color(0xFF5F6A71)
+val Primary = Color(0xFF5F6A71)
+val Surface = Color(0xFFc9d7dd)
+val SurfaceVariant = Color(0xFF829da9)
+val OnSurface = Color(0xFF5f6a71)
+val OnSurfaceVariant = Color(0xFF5f6a71)
+
+//val BackgroundDark
+//val PrimaryDark
+//val SurfaceDark
+//val OnSurfaceDark
+//val OnSurfaceVariant
