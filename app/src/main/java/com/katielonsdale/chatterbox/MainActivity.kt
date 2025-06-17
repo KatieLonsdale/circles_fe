54e5a3b0027cc8ec1f51e7facc2dd19aee2a7c87
katielonsdale <katie952@gmail.com>
2025-06-16 21:26:28 -0600

    switch to CB theme

--- app/src/main/java/com/katielonsdale/chatterbox/MainActivity.kt
+++ app/src/main/java/com/katielonsdale/chatterbox/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.katielonsdale.chatterbox
 
-import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.MaterialTheme
-import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
+import com.katielonsdale.chatterbox.theme.ChatterBoxTheme
 import com.katielonsdale.chatterbox.ui.MainScreen
 import com.katielonsdale.chatterbox.utils.NotificationsManager
 
@@ -35,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         notificationsPermissionManager.checkPushNotificationPermissions()
 
         setContent {
-            MaterialTheme {
+            ChatterBoxTheme {
                 MainScreen(
                     route = intent?.getStringExtra("route"),
                     circleId = intent?.getStringExtra("circleId"),
