95112269c6069d067bbc4c8e3a9b62dc482084d9
katielonsdale <katie952@gmail.com>
2025-06-17 12:28:56 -0600

    update back button

--- app/src/main/java/com/katielonsdale/chatterbox/ui/components/BackButton.kt
+++ app/src/main/java/com/katielonsdale/chatterbox/ui/components/BackButton.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -12,31 +13,32 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.katielonsdale.chatterbox.R
+import com.katielonsdale.chatterbox.theme.ChatterBoxTheme
 
 // Box for back arrow
 @Composable
 fun BackButton(
     onClickBack: () -> Unit
 ){
-    Box() {
-        IconButton(
-            onClick = {
-                onClickBack()
-            },
+    IconButton(
+        onClick = {
+            onClickBack()
+        },
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_back),
+            contentDescription = "Back",
             modifier = Modifier
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_back), // Use your back arrow drawable
-                contentDescription = "Back",
-                modifier = Modifier.align(Alignment.TopStart)
-                    .minimumInteractiveComponentSize(),
-            )
-        }
+                .minimumInteractiveComponentSize(),
+            tint = MaterialTheme.colorScheme.primary
+        )
     }
 }
 
 @Composable
-@Preview(showBackground = true)
+@Preview(apiLevel = 34, showBackground = true)
 fun BackButtonPreview(){
-    BackButton(onClickBack = {})
+    ChatterBoxTheme {
+        BackButton(onClickBack = {})
+    }
 }
