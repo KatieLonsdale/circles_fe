863c82828937729a120d8eccc0099f95fc5183f5
katielonsdale <katie952@gmail.com>
2025-06-21 12:14:46 -0600

    resize and add border to nav bar

--- app/src/main/java/com/katielonsdale/chatterbox/ui/MainScreen.kt
+++ app/src/main/java/com/katielonsdale/chatterbox/ui/MainScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
