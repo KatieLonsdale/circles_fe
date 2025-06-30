2c550f8686f6c586cb5cab2bd59d394b3e0a4841
katielonsdale <katie952@gmail.com>
2025-06-30 17:03:49 -0600

    extract new option icon to composable

--- app/src/main/java/com/katielonsdale/chatterbox/ui/CreateNewScreen.kt
+++ app/src/main/java/com/katielonsdale/chatterbox/ui/CreateNewScreen.kt
@@ -1,7 +1,6 @@
 package com.katielonsdale.chatterbox.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import com.katielonsdale.chatterbox.R
@@ -19,12 +17,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.katielonsdale.chatterbox.theme.ChatterBoxTheme
-import androidx.compose.material3.Icon
-import androidx.compose.ui.draw.drawBehind
+import com.katielonsdale.chatterbox.ui.components.NewOptionIcon
+
 
 
 @Composable
@@ -85,40 +82,6 @@ fun CreateNewScreen(
                 }
             }
         }
-    }
-}
-
-@Composable
-fun NewOptionIcon(
-    action: () -> Unit,
-    icon: Int,
-    label: String,
-){
-    val canvasColor = MaterialTheme.colorScheme.surfaceVariant.copy(
-        alpha = 0.5F
-    )
-    Column(
-        modifier = Modifier
-            .clickable {action()}
-            .size(150.dp)
-            .drawBehind {
-                drawCircle (
-                    color = canvasColor,
-                    radius = 200F
-                )
-            },
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Icon(
-            painter = painterResource(id = icon),
-            contentDescription = "New post"
-        )
-        Text(
-            text = label,
-            color = MaterialTheme.colorScheme.primary,
-            style = MaterialTheme.typography.titleSmall
-        )
     }
 }
 
