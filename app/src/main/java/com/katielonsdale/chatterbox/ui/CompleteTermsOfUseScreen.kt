e35978e4b37b552e0ea7e31b17e1b9bd8e0ad951
katielonsdale <katie952@gmail.com>
2025-07-07 11:48:14 -0600

    remove unused imports

--- app/src/main/java/com/katielonsdale/chatterbox/ui/CompleteTermsOfUseScreen.kt
+++ app/src/main/java/com/katielonsdale/chatterbox/ui/CompleteTermsOfUseScreen.kt
@@ -1,14 +1,12 @@
 package com.katielonsdale.chatterbox.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
