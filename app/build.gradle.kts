4f841037638ea3bc46c0bf006c420bd27adbc874
katielonsdale <katie952@gmail.com>
2025-07-07 12:20:29 -0600

    update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.katielonsdale.chatterbox"
         minSdk = 28
         targetSdk = 34
-        versionCode = 27
-        versionName = "4.0.3"
+        versionCode = 28
+        versionName = "5.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
